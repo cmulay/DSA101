e9a0d0daaf4d988abfa6d982c6b2ffe4eb0df240
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-06-11 15:39:13 +0530

    added searching and sorting algorithms for python

---
+++ python/searching-algorithms/linear_search.py
@@ -0,0 +1,19 @@
+# author : mani-barathi
+
+def linear_search(arr,search):
+	for i in range(len(arr)):
+		if arr[i] == search:
+			return i
+	return None
+
+
+arr = [0,1,4,6,5,7,3,8,2,9]
+search=int(input('Enter element to search: '))
+
+result = linear_search(arr,search)
+
+if result is None:
+	print(f"{search} does'nt exists in the array")
+else:
+	print(f"{search} exists in the index {result}")
+
